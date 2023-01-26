b1219854abdabcefab32f7e152650f69c3524de6
anton startsev <anton.startsev@osome.com>
2023-01-26 21:16:29 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Configuration, OpenAIApi } from "openai";
+
+function App() {
+  return (
+    <div className="App">
+      Johnny Ai
+    </div>
+  );
+}
+
+export default App;
